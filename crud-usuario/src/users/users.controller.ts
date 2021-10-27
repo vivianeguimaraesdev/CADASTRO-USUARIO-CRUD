41330087a7d9bafe86a88f1c076a321c184fcbab
Viviane Guimarães <83373229+vivianeguimaraesdev@users.noreply.github.com>
2021-10-27 15:21:53 -0300

    CRUD aula 02

---
+++ crud-usuario/src/users/users.controller.ts
@@ -0,0 +1,40 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete, NotFoundException } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { NotFoundError } from 'rxjs';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.usersService.create(createUserDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.usersService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: number) {
+    const user = this.usersService.findOne(id);
+
+    if (!user){
+      throw new NotFoundException('User does not exist.');
+    }
+    return user;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: number, @Body() updateUserDto: UpdateUserDto) {
+    return this.usersService.update(id, updateUserDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: number) {
+    return this.usersService.remove(id);
+  }
+}
---
+++ crud-usuario/src/users/users.service.ts
@@ -0,0 +1,56 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+@Injectable()
+export class UsersService {
+  private users: User[] = [];
+
+
+  create(createUserDto: CreateUserDto) {
+    const currentMaxId = this.users[this.users.length -1]?.id || 0;
+
+    const id = currentMaxId + 1;
+
+    const user = {
+      id,
+      ...createUserDto,
+    };
+
+    this.users.push(user);
+    return user;
+  }
+
+  findAll() {
+    return this.users;
+  }
+
+  findOne(id: number) {
+    const index = this.users.findIndex ( (user) => user.id === id);
+    
+    return this.users[index];
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+
+    const user = this.findOne(id);
+
+    const newUser = {
+      ...user,
+      ...updateUserDto,
+    };
+
+    const index = this.users.findIndex ( (user) => user.id === id);
+
+    this.users[index] = newUser;
+
+    return newUser;
+  }
+
+  remove(id: number) {
+    const index = this.users.findIndex ( (user) => user.id === id);
+
+    this.users.splice(index, 1);
+    return this.users;
+  }
+}
